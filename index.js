4dde40d8b2f9300ebd257e95489246f79a3b73ca
Iryna Stein <isophiestein@gmail.com>
2021-04-14 14:05:33 -0600

    Created green mushroom

---
+++ index.js
@@ -0,0 +1,8 @@
+let imageChange2 = document.getElementById('green-m');
+imageChange2.addEventListener('mouseover', function() {
+  imageChange2.src = "images/PurpleMushroom.png";
+});
+
+imageChange2.addEventListener('mouseout', function() {
+    imageChange2.src = "images/GreenMushroom.png";
+  });
